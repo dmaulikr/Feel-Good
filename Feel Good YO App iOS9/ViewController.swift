1aeae53b292e0b85992ceff868bc94923c948ce1
tmehta2442 <tmehta@post.harvard.edu>
2016-02-02 23:54:02 -0800

    ayout, buttons and labels gtg

--- Feel Good YO App iOS9/ViewController.swift
+++ Feel Good YO App iOS9/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMotivationalQuote: UILabel!
+    @IBOutlet weak var btnAnotherQuoteOUTLET: UIButton!
+    
+    var quote = ["You can get it done!", "I beleive in you!", "Carpe Diem"]
+    var numberQuote = 0
+    var buttonText = ["Quote ME", "Inspire Me", "Get it done"]
+    var buttonNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnAnotherQuote(sender: UIButton) {
+        selectQuote()
+        changeButtonText()
+    }
+    
+    func selectQuote(){
+        printQuote()
+    
+    }
 
+    func printQuote(){
+    
+    }
+    
+    func changeButtonText(){
+    
+    }
 }
 
